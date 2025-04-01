eba6604bfcec3f980dc39f6f0a5bff49db1ad4da
EdisDavid <davidedisdev@gmail.com>
2025-03-31 19:17:26 -0500

    Favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rudolf-Motors</title>
-
-
-
+    <title>Rudolf Motor</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    
     @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx'])
