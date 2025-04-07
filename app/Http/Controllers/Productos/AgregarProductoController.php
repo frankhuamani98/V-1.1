78d5404da24ff0df25707ae5bb857e9473c27b37
frankhuamani98 <frankhuamani7602@gmail.com>
2025-04-07 08:47:56 -0500

    Your commit message here

--- app/Http/Controllers/Productos/AgregarProductoController.php
+++ app/Http/Controllers/Productos/AgregarProductoController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Productos;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Producto;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class AgregarProductoController extends Controller
@@ -10,5 +13,7 @@ class AgregarProductoController extends Controller
     public function index()
     {
         return Inertia::render('Dashboard/Productos/AgregarProducto');
+        }
+    
     }
-}
+
