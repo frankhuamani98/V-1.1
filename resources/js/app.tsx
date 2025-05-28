5335672620c3d761302e7849f8e31b1c5ff8a38f
EdisDavid <davidedisdev@gmail.com>
2025-05-28 10:37:46 -0500

    Gobal

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,6 +5,8 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'sonner';
+import WelcomeAlert from '@/Components/WelcomeAlert';
+import WhatsAppButton from '@/Components/WhatsAppButton';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -27,6 +29,7 @@ createInertiaApp({
                     expand={true}
                     className="!top-[3.5rem] sm:!top-[4rem]"
                 />
+                <WhatsAppButton />
             </>
         );
     },
